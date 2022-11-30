0df157f919c8226138501cc23a79265ba007b5cd
taufiqmahdi <57888672+taufiqmahdi@users.noreply.github.com>
2022-11-30 20:22:43 +0700

    Edited router of react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Dashboard from "./pages/Dashboard/Dashboard";
 import AddActivity from "./pages/Dashboard/Features/AddActivity";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./pages/Error/ErrorPage";
 import MainPage from "./pages/Dashboard/MainPage";
 import Profile from "./pages/Dashboard/Profile/Profile";
@@ -10,7 +10,7 @@ import EditProfileModal from "./pages/Modal/EditProfileModal";
 import Cashflow from "./pages/Dashboard/Cashflow/Cashflow";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       element: <RequireAuth />,
       children: [
