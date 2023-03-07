644e4c3ee14ff4a00591f934bff1a6c03f661bbd
taufiqmahdi <57888672+taufiqmahdi@users.noreply.github.com>
2023-03-07 13:55:24 +0700

    update navbar
    update navbar and add drawer for mobile breakpoints

--- src/pages/Component/Drawer/NavbarDrawer.js
+++ src/pages/Component/Drawer/NavbarDrawer.js
@@ -1,41 +1,41 @@
-import { Divider, Flex, Heading, Image, Avatar } from "@chakra-ui/react";
-import { BellIcon, HamburgerIcon } from "@chakra-ui/icons";
-import React, { useState } from "react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  Input,
+  Image,
+  useDisclosure,
+  Divider,
+} from "@chakra-ui/react";
+import React from "react";
 import { Link } from "react-router-dom";
-import NavbarDrawer from "./Drawer/NavbarDrawer";
 
-const Navbar = ({ user, onUserChange }) => {
-  const handleUserChange = (user) => {
-    onUserChange(user);
-  };
+const NavbarDrawer = ({ isSidebarOpen, onSidebarChange, onLogout }) => {
+  const btnRef = React.useRef();
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const handleSidebarOpenChange = (value) => {
-    setIsSidebarOpen(value);
-  };
-
-  const logout = () => {
-    localStorage.removeItem("user");
-    handleUserChange("");
+  const handleSidebarOpen = () => {
+    onSidebarChange((prevState) => !prevState);
   };
 
   return (
-    <Flex zIndex="2">
-      <Flex
-        className="navbar"
-        align="center"
-        bgColor="white"
-        justify="space-between"
-        w="175px"
-        h="100vh"
-        direction="column"
-        boxShadow="xl"
-        pos="fixed"
-        zIndex="3"
-        left="0"
-        display={{ base: "none", lg: "flex" }}
-      >
-        <Flex direction="column" w="100%" align="center" h="100%">
+    <Drawer
+      isOpen={isSidebarOpen}
+      placement="left"
+      onClose={onSidebarChange}
+      finalFocusRef={btnRef}
+      size={'xs'}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>
           <Link to="/">
             <Flex
               align="center"
@@ -59,7 +59,10 @@ const Navbar = ({ user, onUserChange }) => {
               </Heading>
             </Flex>
           </Link>
-          <Divider w="70%" my="10px" />
+          <Divider my="10px" />
+        </DrawerHeader>
+
+        <DrawerBody>
           <Flex direction="column" w="100%" h="100%" justify="flex-start">
             <Flex direction="column" w="100%">
               <Link to="/">
@@ -110,8 +113,10 @@ const Navbar = ({ user, onUserChange }) => {
               </Link>
             </Flex>
           </Flex>
+        </DrawerBody>
+
+        <DrawerFooter>
           <Flex direction="column" w="100%" h="100%" justify="flex-end">
-            {/* <Link to="/login"> */}
             <Flex
               as="button"
               p="15px"
@@ -120,65 +125,17 @@ const Navbar = ({ user, onUserChange }) => {
               h="3rem"
               align="center"
               justify="flex-start"
-              onClick={logout}
+              onClick={onLogout}
             >
               <Heading as="h3" size="sm">
                 Logout
               </Heading>
             </Flex>
-            {/* </Link> */}
           </Flex>
-        </Flex>
-      </Flex>
-      <Flex
-        bgColor="gray.100"
-        align="center"
-        justify="space-between"
-        h="75px"
-        p="25px"
-        w="100%"
-        pos="fixed"
-        pl={{ base: "25px", lg: "200px" }}
-      >
-        <Flex fontSize="xl" fontWeight="bold" align="center" gap={6}>
-          <HamburgerIcon
-            _hover={{ cursor: "pointer" }}
-            onClick={handleSidebarOpenChange}
-            display={{ base: "block", lg: "none" }}
-          />
-          Hello, {user.firstName + " " + user.lastName}
-          <NavbarDrawer
-            onSidebarChange={handleSidebarOpenChange}
-            isSidebarOpen={isSidebarOpen}
-            onLogout={logout}
-          />
-        </Flex>
-        <Flex align="center">
-          <Flex
-            mr="15px"
-            h="45px"
-            w="45px"
-            borderRadius="8px"
-            bgColor="white"
-            align="center"
-            justify="center"
-          >
-            <BellIcon w="25px" h="25px" />
-          </Flex>
-          <Flex mr="15px">{user.firstName + " " + user.lastName}</Flex>
-          <Flex>
-            <Avatar
-              name={user.firstName + " " + user.lastName}
-              // src="https://bit.ly/dan-abramov"
-              borderRadius="8px"
-              h="45px"
-              w="45px"
-            />
-          </Flex>
-        </Flex>
-      </Flex>
-    </Flex>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
-export default Navbar;
+export default NavbarDrawer;
